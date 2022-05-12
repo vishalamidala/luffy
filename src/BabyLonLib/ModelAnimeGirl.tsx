1351f8b542966f217e476980d8399d05ae0cc250
vishalamidala <89906967+vishalamidala@users.noreply.github.com>
2022-05-12 16:41:45 +0200

    walking stable

--- src/BabyLonLib/ModelAnimeGirl.tsx
+++ src/BabyLonLib/ModelAnimeGirl.tsx
@@ -9,7 +9,7 @@ const modelAssetTasks = [
   {
     taskType: TaskType.Mesh,
     rootUrl: `${baseUrl3}`,
-    sceneFilename: 'animegirl.glb',
+    sceneFilename: 'girl.glb',
     name: 'animegirl',
   },
 ] as any;
@@ -31,7 +31,7 @@ export const ModelAnimeGirl = (props: {
     const loadAsset = () => {
       const animegirlTask: any = assetManagerResult.taskNameMap['animegirl'];
       const animegirl = animegirlTask.loadedMeshes[0];
-      animegirl.scaling = new Vector3(0.8, 0.8, 0.8);
+      animegirl.scaling = new Vector3(0.1, 0.1, 0.1);
       animegirlTask.loadedMeshes[0].position = new Vector3(0, 0, 0);
       const heroSpeed = 0.04;
       const heroSpeedBackwards = 0.01;
@@ -47,102 +47,106 @@ export const ModelAnimeGirl = (props: {
       // const camera: any = scene?.activeCamera;
       // camera.setTarget(animegirlTask.loadedMeshes[0].position);
 
-      const idleAnimation = animegirlTask.loadedAnimationGroups.filter(
-        (animations: any) => animations.name.includes('idle')
-      )[0];
-      const armsAnimation = animegirlTask.loadedAnimationGroups.filter(
-        (animations: any) => animations.name.includes('arms_hip_hp_dance')
-      )[0];
-      const breakAnimation = animegirlTask.loadedAnimationGroups.filter(
-        (animations: any) => animations.name.includes('break_dance')
-      )[0];
-      const kickAnimation = animegirlTask.loadedAnimationGroups.filter(
-        (animations: any) => animations.name.includes('kick')
-      )[0];
-      const walkingAnimation = animegirlTask.loadedAnimationGroups.filter(
-        (animations: any) => animations.name.includes('walking')
-      )[0];
-      const walkingTurnAnimation = animegirlTask.loadedAnimationGroups.filter(
-        (animations: any) => animations.name.includes('walking_turn_180')
-      )[0];
-      if (props.animationName === 'idle') {
-        kickAnimation.stop();
-        armsAnimation.stop();
-        idleAnimation.start(
-          true,
-          1.0,
-          idleAnimation.from,
-          idleAnimation.to,
-          false
-        );
-      }
-      if (props.animationName === 'arms_hip_hp_dance')
-        armsAnimation.start(
-          true,
-          1.0,
-          armsAnimation.from,
-          armsAnimation.to,
-          false
-        );
-      if (props.animationName === 'kick') {
-        idleAnimation.stop();
-        armsAnimation.stop();
-        kickAnimation.start(
-          true,
-          1.0,
-          kickAnimation.from,
-          kickAnimation.to,
-          false
-        );
-      }
-      if (props.animationName === 'walking') {
-        idleAnimation.stop();
-        armsAnimation.stop();
-        kickAnimation.stop();
-        walkingAnimation.start(
-          true,
-          1.0,
-          walkingAnimation.from,
-          walkingAnimation.to,
-          false
-        );
-      }
-      if (props.animationName === 'walking_turn_180') {
-        idleAnimation.stop();
-        armsAnimation.stop();
-        kickAnimation.stop();
-        walkingTurnAnimation.start(
-          true,
-          1.0,
-          walkingTurnAnimation.from,
-          walkingTurnAnimation.to,
-          false
-        );
-      }
-      if (props.animationName === 'break_dance') {
-        idleAnimation.stop();
-        armsAnimation.stop();
-        kickAnimation.stop();
-        breakAnimation.start(
-          true,
-          1.0,
-          breakAnimation.from,
-          breakAnimation.to,
-          false
-        );
-      }
-      if (props.animationName === 'stop') {
-        idleAnimation.stop();
-        armsAnimation.stop();
-        kickAnimation.stop();
-        walkingAnimation.stop();
-        walkingTurnAnimation.stop();
-        breakAnimation.stop();
-      }
+      // const idleAnimation = animegirlTask.loadedAnimationGroups.filter(
+      //   (animations: any) => animations.name.includes('idle')
+      // )[0];
+      // const armsAnimation = animegirlTask.loadedAnimationGroups.filter(
+      //   (animations: any) => animations.name.includes('arms_hip_hp_dance')
+      // )[0];
+      // const breakAnimation = animegirlTask.loadedAnimationGroups.filter(
+      //   (animations: any) => animations.name.includes('break_dance')
+      // )[0];
+      // const kickAnimation = animegirlTask.loadedAnimationGroups.filter(
+      //   (animations: any) => animations.name.includes('kick')
+      // )[0];
+      // const walkingAnimation = animegirlTask.loadedAnimationGroups.filter(
+      //   (animations: any) => animations.name.includes('walking')
+      // )[0];
+      // const walkingTurnAnimation = animegirlTask.loadedAnimationGroups.filter(
+      //   (animations: any) => animations.name.includes('walking_turn_180')
+      // )[0];
+      // if (props.animationName === 'idle') {
+      //   kickAnimation.stop();
+      //   armsAnimation.stop();
+      //   idleAnimation.start(
+      //     true,
+      //     1.0,
+      //     idleAnimation.from,
+      //     idleAnimation.to,
+      //     false
+      //   );
+      // }
+      // if (props.animationName === 'arms_hip_hp_dance')
+      //   armsAnimation.start(
+      //     true,
+      //     1.0,
+      //     armsAnimation.from,
+      //     armsAnimation.to,
+      //     false
+      //   );
+      // if (props.animationName === 'kick') {
+      //   idleAnimation.stop();
+      //   armsAnimation.stop();
+      //   kickAnimation.start(
+      //     true,
+      //     1.0,
+      //     kickAnimation.from,
+      //     kickAnimation.to,
+      //     false
+      //   );
+      // }
+      // if (props.animationName === 'walking') {
+      //   idleAnimation.stop();
+      //   armsAnimation.stop();
+      //   kickAnimation.stop();
+      //   walkingAnimation.start(
+      //     true,
+      //     1.0,
+      //     walkingAnimation.from,
+      //     walkingAnimation.to,
+      //     false
+      //   );
+      // }
+      // if (props.animationName === 'walking_turn_180') {
+      //   idleAnimation.stop();
+      //   armsAnimation.stop();
+      //   kickAnimation.stop();
+      //   walkingTurnAnimation.start(
+      //     true,
+      //     1.0,
+      //     walkingTurnAnimation.from,
+      //     walkingTurnAnimation.to,
+      //     false
+      //   );
+      // }
+      // if (props.animationName === 'break_dance') {
+      //   idleAnimation.stop();
+      //   armsAnimation.stop();
+      //   kickAnimation.stop();
+      //   breakAnimation.start(
+      //     true,
+      //     1.0,
+      //     breakAnimation.from,
+      //     breakAnimation.to,
+      //     false
+      //   );
+      // }
+      // if (props.animationName === 'stop') {
+      //   idleAnimation.stop();
+      //   armsAnimation.stop();
+      //   kickAnimation.stop();
+      //   walkingAnimation.stop();
+      //   walkingTurnAnimation.stop();
+      //   breakAnimation.stop();
+      // }
 
       // keydown events excuted every frame
       if (scene) {
-        const walk = scene.getAnimationGroupByName('walking');
+        console.log();
+        const walk = scene.getAnimationGroupByName('Walking');
+        const walkBackAnim = scene.getAnimationGroupByName('WalkingBack');
+        const idleAnim = scene.getAnimationGroupByName('Idle');
+
         let inputMap = {} as any;
         scene.actionManager = new ActionManager(scene);
         scene.actionManager.registerAction(
@@ -163,6 +167,12 @@ export const ModelAnimeGirl = (props: {
             );
             keydown = true;
           }
+          if (inputMap['s']) {
+            animegirl.moveWithCollisions(
+              animegirl.forward.scaleInPlace(-heroSpeedBackwards)
+            );
+            keydown = true;
+          }
           if (inputMap['a']) {
             animegirl.rotate(Vector3.Up(), -heroRotationSpeed);
             keydown = true;
@@ -175,25 +185,32 @@ export const ModelAnimeGirl = (props: {
           if (keydown) {
             if (!animating) {
               animating = true;
-              walk?.start(true, 2, walk.from, walk.to, false);
+              if (inputMap['s']) {
+                //Walk backwards
+                walkBackAnim?.start(
+                  true,
+                  1.0,
+                  walkBackAnim.from,
+                  walkBackAnim.to,
+                  false
+                );
+              } else {
+                walk?.start(true, 1, walk.from, walk.to, false);
+              }
             }
           } else {
             if (animating) {
               //Default animation is idle when no key is down
-              idleAnimation.start(
-                true,
-                1.0,
-                idleAnimation.from,
-                idleAnimation.to,
-                true
-              );
+              idleAnim?.start(true, 1.0, idleAnim.from, idleAnim.to, true);
 
-              //Stop all animations besides Idle Anim when no key is down
-              armsAnimation.stop();
-              kickAnimation.stop();
-              walkingAnimation.stop();
-              walkingTurnAnimation.stop();
-              breakAnimation.stop();
+              // //Stop all animations besides Idle Anim when no key is down
+              // armsAnimation.stop();
+              // kickAnimation.stop();
+              // walkingAnimation.stop();
+              // walkingTurnAnimation.stop();
+              // breakAnimation.stop();
+              walk?.stop();
+              walkBackAnim?.stop();
 
               //Ensure animation are played only once per rendering loop
               animating = false;
